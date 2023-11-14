319d1cda30e868d6273086d321effd2a176e9b74
mhaddouc1 <m.haddouch1@student.avans.nl>
2023-11-14 15:41:27 +0100

    individueel project bootstrap geinstalleerd met en header en footer aangemaakt

--- apps/streamtaku-web/src/app/app.component.ts
+++ apps/streamtaku-web/src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { NxWelcomeComponent } from './nx-welcome.component';
+import { UiModule } from '@avans-nx-individueel/ui';
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule],
+  imports: [RouterModule, UiModule],
   selector: 'avans-nx-individueel-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
---
+++ libs/streamtaku/ui/src/lib/footer/footer.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'avans-nx-individueel-footer',
+  templateUrl: './footer.component.html',
+  styleUrls: ['./footer.component.css'],
+})
+export class FooterComponent {}
