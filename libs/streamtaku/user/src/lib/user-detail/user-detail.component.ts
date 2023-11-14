9b102a65723a3ef6186cd1a4b66dcb545c860994
mhaddouc1 <m.haddouch1@student.avans.nl>
2023-11-14 16:54:20 +0100

    users mappen aangemaakt en nu te zien op github

---
+++ libs/streamtaku/user/src/lib/user-detail/user-detail.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'avans-nx-individueel-user-detail',
+  templateUrl: './user-detail.component.html',
+  styleUrls: ['./user-detail.component.css'],
+})
+export class UserDetailComponent {}
---
+++ libs/streamtaku/user/src/lib/user-list/user-list.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'avans-nx-individueel-user-list',
+  templateUrl: './user-list.component.html',
+  styleUrls: ['./user-list.component.css'],
+})
+export class UserListComponent {}
