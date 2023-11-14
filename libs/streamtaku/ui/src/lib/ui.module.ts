9a0ff5cdf671cb41c6d73d0497e368af618487b7
mhaddouc1 <m.haddouch1@student.avans.nl>
2023-11-14 16:50:11 +0100

    users mappen aangemaakt

--- libs/streamtaku/ui/src/lib/ui.module.ts
+++ libs/streamtaku/ui/src/lib/ui.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [CommonModule, NgbModule],
   declarations: [HeaderComponent, FooterComponent],
   exports: [HeaderComponent, FooterComponent],
 })
