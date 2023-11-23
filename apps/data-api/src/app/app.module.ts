13efaa0b49d1b75d7f2c71d6c61af9fc9e2cda96
mhaddouc1 <m.haddouch1@student.avans.nl>
2023-11-23 11:36:00 +0100

    data api proberen te deployen

--- apps/data-api/src/app/app.module.ts
+++ apps/data-api/src/app/app.module.ts
@@ -3,15 +3,15 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
+import { UserModule } from '@avans-nx-individueel/backend/user';
 
 @Module({
   imports: [
     MongooseModule.forRoot(
-      process.env['MONGODB_CONNECTION_STRING'] ??
-        'mongodb://localhost/streamtaku'
+       process.env['MONGODB_CONNECTION_STRING'] ??
+      'mongodb://127.0.0.1:27017/streamtaku'
     ),
+    UserModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
