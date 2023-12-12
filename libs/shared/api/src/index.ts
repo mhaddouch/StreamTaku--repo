7f3ee6a7a01600b5aeb8f9ff842d7d8776fa96fa
mhaddouc1 <m.haddouch1@student.avans.nl>
2023-12-12 13:12:58 +0100

    andere interface entiteiten aangemaakt

--- libs/shared/api/src/index.ts
+++ libs/shared/api/src/index.ts
@@ -1,3 +1,5 @@
 export * from './lib/models/anime.interface';
 export * from './lib/models/user.interface';
 export * from './lib/models/auth.interface';
+export * from './lib/models/episode.interface';
+export * from './lib/models/comment.interface';
--- libs/shared/api/src/lib/models/anime.interface.ts
+++ libs/shared/api/src/lib/models/anime.interface.ts
@@ -1,1 +1,26 @@
-export interface IAnime {}
+import { IEpisode } from './episode.interface';
+import { IUser } from './user.interface';
+
+export enum Genre {
+  action = 'action',
+  adventure = 'adventure',
+  romance = 'romance',
+  comedy = 'comedy',
+  triller = 'triller',
+  horror = 'horror',
+  crime = 'crime',
+  fantasy = 'fantasy',
+  sport = 'sport',
+}
+export interface IAnime {
+  _id: string;
+  title: string;
+  genre: Genre;
+  writer:string;
+  publisher: Pick<IUser, '_id' | 'name'>;
+  publishedDate: Date;
+  episodes: IEpisode[];
+}
+
+export type ICreateAnime = Pick<IAnime, 'title' | 'genre'|'writer'>;
+export type IUpdateAnime = Pick<IAnime, 'title' | 'genre'|'writer'>;
